716ed896b559860aed76b5bfd4bad11f73805afe
Panchal <siddhesh.panchal@capgemini.com>
2021-06-05 11:32:25 +0530

    Search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,13 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	public void login() {
+
+		System.out.println("This is my log in page class");
+	}
+	
+	public void loginSet() {
+
+		System.out.println("This is my log in page Set method");
+	}
 }
